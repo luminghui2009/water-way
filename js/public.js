26b92049077c0488c66dc7159a3c10dfcd6a8ef7
ming1130 <1002253391@qq.com>
2017-08-18 15:36:25 +0800

    第一次提交

---
+++ js/public.js
@@ -0,0 +1,9 @@
+$(".nav_bar a:nth-child(0)").on("click",function(){
+    window.location.href="index.html";
+});
+$(".nav_bar a:nth-child(1)").on("click",function(){
+    window.location.href="news.html";
+});
+$(".nav_bar a:nth-child(2)").on("click",function(){
+    window.location.href="products.html";
+})

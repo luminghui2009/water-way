1676e3cff17556978e367e54141a0c50e296e7f0
ming1130 <1002253391@qq.com>
2017-08-24 21:50:30 +0800

    产品展示图片补充

--- js/public.js
+++
@@ -1,9 +0,0 @@
-$(".nav_bar a:nth-child(0)").on("click",function(){
-    window.location.href="index.html";
-});
-$(".nav_bar a:nth-child(1)").on("click",function(){
-    window.location.href="news.html";
-});
-$(".nav_bar a:nth-child(2)").on("click",function(){
-    window.location.href="products.html";
-})
